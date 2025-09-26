cd187c1f00ceb6f0101e9d879dec627cf986ec18
Unimate <unimategp@gmail.com>
2025-09-26 17:57:16 +0200

    master #comment: - clean-up.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,6 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Workspace } from "../workspace/workspace";
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
 
 export default function Home() {
-  return <Welcome />;
+  return <Workspace />;
 }
